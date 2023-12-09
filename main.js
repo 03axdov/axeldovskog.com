211dd454d4a35fb015b016b4d0111ac86a56b2ed
axeld <axel.dovskog@outlook.com>
2023-12-09 22:20:24 +0100

    Changes to education

--- main.js
+++ main.js
@@ -16,3 +16,26 @@ function addAnimation() {
         })
     })
 }
+
+function CSCourse(title, description) {
+    document.getElementById("cs-title").innerHTML = title;
+    document.getElementById("cs-description").innerHTML = description;
+}
+
+function EDAA60() {
+    title = "Computer Introduction";
+    desc = "Covered the basics of Unix and Linux, LaTeX, as well as Git and GitHub.";
+    CSCourse(title, desc);
+}
+
+function EDAA45() {
+    title = "Introduction to Programming";
+    desc = "Covered the basics of programming algorithms, as well as the programming language Scala.";
+    CSCourse(title, desc);
+}
+
+function FMAB65() {
+    title = "Calculus in One Variable B1";
+    desc = "Mathematics course covering analytical geometry, trigonometry, limit values, etc.";
+    CSCourse(title, desc);
+}
